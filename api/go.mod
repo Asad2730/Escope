f9f6fd5af5dc39142648e19b1931996b3b497497
Asad2730 <asadsajjad777@gmail.com>
2024-01-11 12:10:16 +0500

    1

--- api/go.mod
+++ api/go.mod
@@ -1,6 +1,6 @@
 module github.com/Asad2730/Escope
 
-go 1.21.5
+go 1.21.6
 
 require (
 	github.com/gin-gonic/gin v1.9.1
--- api/models/user.go
+++ api/models/user.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email    string `json:"email" gorm:"primaryKey"`
+	Email    string `json:"email" gorm:"primaryKey;unique"`
 	Password string `json:"password"`
 	FaceID   []byte `json:"face_id"`
+	UserID   uint   `json:"user_id" gorm:"autoIncrement"`
 }
